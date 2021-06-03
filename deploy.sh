9d1c32dcc6ab80c169ee1eaa6cb12b8bc80ce50b
“kii-chan-iinegit config --global user.email “kaichen1993@hotmail.com <kaichen1993@hotmail.com>
2021-06-03 15:54:01 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+date=`date +"%Y-%m-%d %H:%M:%S"`
+commitmsg='Site updated: '$date
+# yarn build
+git add -A
+git commit -m "$commitmsg"
+git config --local user.name kii-chan-iine
+git config --local user.email kaichen1993@hotmail.com
+
+# 如果发布到 https://<USERNAME>.github.io
+git push -f git@github.com:kii-chan-iine/kii-chan-iine.github.io.git
